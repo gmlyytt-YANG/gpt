35b15e18094a46ed8e733da08531119e874c3bab
gmlyytt-YANG <gmlyytt@outlook.com>
2023-06-14 22:22:58 +0800

    add code

---
+++ util.py
@@ -0,0 +1,154 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import tensorflow as tf 
+import random
+import os
+import sys
+from numpy import arange
+import math
+import pyecharts
+import sys,base64,urllib,re
+import multiprocessing
+import warnings 
+from optparse import OptionParser
+import logging
+import logging.config
+import time
+
+import tensorflow_datasets as tfds
+import tensorflow.keras.layers as layers
+
+import time
+import matplotlib.pyplot as plt
+import jieba
+
+def get_angles(pos, i, d_model):
+    # 这里的i等价与上面公式中的2i和2i+1
+    angle_rates = 1 / np.power(10000, (2*(i // 2))/ np.float32(d_model))
+    return pos * angle_rates
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                           np.arange(d_model)[np.newaxis,:],
+                           d_model)
+    # TODO: 确认正确性
+    sines = np.sin(angle_rads[:, 0::2])
+    cones = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = np.concatenate([sines, cones], axis=-1)
+    pos_encoding = pos_encoding[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+def create_padding_mark(seq):
+    # 获取为0的padding项
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+
+    # 扩充维度以便用于attention矩阵
+    return seq[:, np.newaxis, np.newaxis, :] # (batch_size,1,1,seq_len)
+
+
+def create_look_ahead_mark(size):
+    # 1 - 对角线和取下三角的全部对角线（-1->全部）
+    # 这样就可以构造出每个时刻未预测token的掩码
+    mark = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mark  # (seq_len, seq_len)
+
+
+def scaled_dot_product_attention(q, k, v, mask):
+    # query key 相乘获取匹配关系
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+
+    # 使用dk进行缩放
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # 掩码
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    # 通过softmax获取attention权重
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    # attention 乘上value
+    output = tf.matmul(attention_weights, v) # （.., seq_len_v, depth）
+
+    return output, attention_weights
+
+# 构造mutil head attention层
+class MutilHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MutilHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        # d_model 必须可以正确分为各个头
+        assert d_model % num_heads == 0
+        # 分头后的维度
+        self.depth = d_model // num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        # 分头, 将头个数的维度 放到 seq_len 前面
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+
+        # 分头前的前向网络，获取q、k、v语义
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)
+        v = self.wv(v)
+
+        # 分头
+        q = self.split_heads(q, batch_size) # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_v, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+
+        # 通过缩放点积注意力层
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+        q, k, v, mask)
+        # 把多头维度后移
+        scaled_attention = tf.transpose(scaled_attention, [0, 2, 1, 3]) # (batch_size, seq_len_v, num_heads, depth)
+
+        # 合并多头
+        concat_attention = tf.reshape(scaled_attention, 
+                                      (batch_size, -1, self.d_model))
+
+        # 全连接重塑
+        output = self.dense(concat_attention)
+        return output, attention_weights
+
+
+class LayerNormalization(tf.keras.layers.Layer):
+    def __init__(self, epsilon=1e-6, **kwargs):
+        self.eps = epsilon
+        super(LayerNormalization, self).__init__(**kwargs)
+    def build(self, input_shape):
+        self.gamma = self.add_weight(name='gamma', shape=input_shape[-1:],
+                                     initializer=tf.ones_initializer(), trainable=True)
+        self.beta = self.add_weight(name='beta', shape=input_shape[-1:],
+                                    initializer=tf.zeros_initializer(), trainable=True)
+        super(LayerNormalization, self).build(input_shape)
+    def call(self, x):
+        mean = tf.keras.backend.mean(x, axis=-1, keepdims=True)
+        std = tf.keras.backend.std(x, axis=-1, keepdims=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    
+def point_wise_feed_forward_network(d_model, diff):
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(diff, activation='relu'),
+        tf.keras.layers.Dense(d_model)
+    ])
